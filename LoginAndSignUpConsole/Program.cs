1fe106bcaad588a9ae54a313957e7fdb8a759e63
MohammadReza Balakhaniyan <m.balakhaniyan@gmail.com>
2023-02-26 00:57:42 +0330

    addConsoleAndInteractive

---
+++ LoginAndSignUpConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using LoginAndSignUp;
+
+// var config = new Password.Config()
+// {
+//     MinLength = 8,
+//     RequiredDigit = true,
+//     RequiredLowercase = true,
+//     RequiredUppercase = true,
+//     RequiredSpecial = true
+// };
+
+// var result = Password.CheckPassword("1abcdefg", config);
+
+// Console.WriteLine(result);
+
+Email.SendVerificationCode("m.balakhaniyan@gmail.com");
